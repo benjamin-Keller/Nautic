2cfb5e5d6507094792f828f3a1ac876c0abd235e
benjamin-Keller <zhero240@gmail.com>
2025-10-19 23:19:59 +0200

    Adds basic docker integration for real-time metrics

--- Nautic/Program.cs
+++ Nautic/Program.cs
@@ -1,4 +1,5 @@
 using Nautic.Components;
+using Nautic.Services;
 
 namespace Nautic
 {
@@ -11,6 +12,8 @@ namespace Nautic
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
+
+            builder.Services.AddScoped<DockerService>();
 
             var app = builder.Build();
 
